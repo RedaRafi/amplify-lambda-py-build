e063a750e582591d72aab925c077d10b255917cf
David Moser <davmoser@amazon.com>
2020-06-19 14:31:11 -0700

    added comments and better formating to the bash script

--- scripts/amplify-py-build.sh
+++ scripts/amplify-py-build.sh
@@ -1,2 +1,13 @@
-docker run -it -v "$PWD"/amplify/backend/function/hellopy/src:/src python:3 bash -c "pip install -r './src/requirements.txt' --upgrade  --target ./src"
+# run the python3 docker container
+# mounting the lambda function's src folder
+# run pip in the container using the lambda function's requirements file and outputting
+# the Linux dependencies to the lambda function's src folder
+
+export PIPENV_VERBOSITY=-1
+
+docker run -it \
+    -v "$PWD"/amplify/backend/function/hellopy/src:/src \
+    python:3 \
+    bash -c "pip install -r './src/requirements.txt' --upgrade --target ./src"
+
 amplify push
