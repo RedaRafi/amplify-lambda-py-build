6e021b744cccb10de7fc3d2ee0473c96c6a41b8d
David Moser <davmoser@amazon.com>
2020-06-19 14:40:32 -0700

    reformat bash script

--- scripts/amplify-py-build.sh
+++ scripts/amplify-py-build.sh
@@ -3,11 +3,11 @@
 # run pip in the container using the lambda function's requirements file and outputting
 # the Linux dependencies to the lambda function's src folder
 
-export PIPENV_VERBOSITY=-1
-
 docker run -it \
     -v "$PWD"/amplify/backend/function/hellopy/src:/src \
     python:3 \
     bash -c "pip install -r './src/requirements.txt' --upgrade --target ./src"
+
+export PIPENV_VERBOSITY=-1
 
 amplify push
